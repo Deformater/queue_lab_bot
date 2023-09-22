79bb3d663045a402b816d4e0c6b6c6e5a6c7a4c7
Deformater <muravenkog@yandex.ru>
2023-09-22 20:10:01 +0300

    feat: add migrations

---
+++ migrations/models/2_20230922200825_None.py
@@ -0,0 +1,25 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "records" (
+    "uuid" UUID NOT NULL  PRIMARY KEY,
+    "student_name" VARCHAR(255) NOT NULL,
+    "student_group" VARCHAR(255) NOT NULL,
+    "lab_name" VARCHAR(255) NOT NULL,
+    "lab_date" DATE NOT NULL,
+    "stream" VARCHAR(20) NOT NULL  DEFAULT 'Change',
+    "datetime" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
